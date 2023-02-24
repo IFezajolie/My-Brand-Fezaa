e4d015b6e6345c458659a8b4b481de3ca3e84d2b
IFezajolie <119081759+IFezajolie@users.noreply.github.com>
2023-02-24 19:59:43 +0200

    Creating an article

--- JS/create.js
+++ JS/create.js
@@ -13,16 +13,6 @@ await fetch('http://localhost:3000/Articles', {
     
     // headers: {'content-Type': 'application/json' }
 })
-const article = document.createElement('div');
-const container = document.querySelector('article_section_container')
-  article.classList.add('article_section_one');
-  article.innerHTML = `
-    <h2>${title}</h2>
-    <p>${body}</p>
-  `;
-
-  // Add new article to article section
-  container.appendChild('article_section_one');
 window.location.replace('/dashport.html');
 button.addEventListener('submit', createPost);
 }
