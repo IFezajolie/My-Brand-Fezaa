668013c8b44abd74447715427ed4be0777e7102c
IFezajolie <119081759+IFezajolie@users.noreply.github.com>
2023-02-27 10:00:30 +0200

    Did it

--- JS/create.js
+++ JS/create.js
@@ -1,11 +1,20 @@
 const form = document.querySelector('form');
 const createArticle = async (e) => {
     e.preventDefault();
+    // const doc = {
+    //     title: form.title.value,
+    //     body: form.body.value,
+        
+    // }
+    // const doc = {
+    //     title: document.querySelector('#title').value,
+    //     body: document.querySelector('#body').value
+    //  }
     const doc = {
         title: form.title.value,
-        body: form.body.value,
-        
+        body: form.body.value
     }
+     
 await fetch('http://localhost:3000/Articles', {
     method: "POST",
     body: JSON.stringify(doc),
@@ -15,4 +24,20 @@ await fetch('http://localhost:3000/Articles', {
 })
 window.location.replace('/dashport.html');
 button.addEventListener('submit', createPost);
-}
+
+// CHAT GPT
+// const form = document.querySelector('form');
+// const createArticle = async (e) => {
+//   e.preventDefault();
+//   const doc = {
+//     title: document.querySelector('#title').value,
+//     body: document.querySelector('#body').value
+//   }
+//   await fetch('http://localhost:3000/Articles', {
+//     method: "POST",
+//     body: JSON.stringify(doc),
+//     headers: {'content-Type': 'application/json' }
+//   })
+//   window.location.replace('/dashport.html');
+// }
+// form.addEventListener('submit', createArticle);
